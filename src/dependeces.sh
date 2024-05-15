b7494dc181922d4f353f514a00516fce678e244b
Marcelo <mrcelo014@gmail.com>
2024-05-15 11:33:08 -0300

    tests

--- src/dependeces.sh
+++ src/dependeces.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
-# Script de automação para montar partições com Rust
+# echo "setting keyword"
+# loadkeys br-abnt2
 
-# Verifica se o usuário é root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Este script precisa ser executado como root"
-    exit 1
+lsblk -l
+echo "disk path"
+read path
+sudo cfdisk "$path"
+lsblk -l
+echo "what is your partition configuration \
+    1 => ext4
+    2 => btrfs
+"
+read type
+echo "format boot partion, enter the path"
+read boot
+mkfs.fat -F 32 "$boot"
+echo "swap? 
+if yes => press the key 1
+if not => press any key"
+read is
+if ["$swap" == 1]; then
+    echo "partition path
+    read swap_path
+    mkswap "$swap_path"
 fi
-
-# Instala o compilador Rust
-pacman -S curl base-devel 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-exec ../target/debug/rustinstallarch
+echo "root path"
+read root_path
+mkfs."$type" "$root_path"
+echo "home? 
+if yes => press the key 1
+if not => press any key"
+read is
+if ["$swap" == 1]; then
+    echo "partition path
+    read home_path
+    mkfs."$type" "$home_path"
+fi
+lsblk -l
