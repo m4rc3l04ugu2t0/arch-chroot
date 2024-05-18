76229ecf4603bc9f10ecbf132519a084d7886abb
Marcelo <mrcelo014@gmail.com>
2024-05-17 22:16:00 -0300

    path

--- src/date_conf.rs
+++ src/date_conf.rs
@@ -2,7 +2,7 @@ use crate::run_commands::run_command;
 
 pub fn date_conf(region: &'static str, cyte: &'static str) {
     let mut command = vec![
-        "ln -sf /usr/share/zoneinfor/",
+        "ln -sf /usr/share/zoneinfo/",
         "",
         "/",
         "",
