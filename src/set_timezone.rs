34b6c17c80d67838453af6525ef2f7f6fbcd8284
Marcelo <mrcelo014@gmail.com>
2024-05-17 22:44:31 -0300

    fix?

--- src/set_timezone.rs
+++ src/set_timezone.rs
@@ -1,6 +1,7 @@
 use crate::run_commands::run_command;
 
 pub fn set_timezone(timezone: &str) {
+    // Executar o comando para configurar o fuso horário
     let command = format!("ln -sf /usr/share/zoneinfo/{} /etc/localtime", timezone);
     let args = [
         "-sf",
