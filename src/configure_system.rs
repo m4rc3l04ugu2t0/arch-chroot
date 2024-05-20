b626f5d20a03b19aa2ee142f90808e5f581651e3
Marcelo <mrcelo014@gmail.com>
2024-05-20 16:51:54 -0300

    fix save state

--- src/configure_system.rs
+++ src/configure_system.rs
@@ -3,14 +3,14 @@ use crate::{
     conf_sys::config_system,
     config_timezone::set_timezone::set_timezone,
     configure_lanaguage::set_language::set_language,
-    run_commands::{correct_errror, is_correctable_error, run_command},
+    run_commands::{correct_errror, is_correctable_error},
 };
 use serde::{Deserialize, Serialize};
 use std::{
     fs::{self, OpenOptions},
     io::{BufReader, Read},
     path::Path,
-    process::{exit, Command},
+    process::exit,
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -73,11 +73,10 @@ fn save_state(state: &State) -> Result<(), String> {
 
     let state_dir = Path::new(state_file).parent().unwrap();
 
-    if !state_dir.exists() {
-        run_command(&mut Command::new("mkdir state.json"));
-        // fs::create_dir(state_dir)
-        //     .map_err(|e| format!("Falha ao criar diretório {}: {}", state_dir.display(), e))?;
-    }
+    // if !state_dir.exists() {
+    //     fs::create_dir(state_dir)
+    //         .map_err(|e| format!("Falha ao criar diretório {}: {}", state_dir.display(), e))?;
+    // }
 
     let file = OpenOptions::new()
         .write(true)
