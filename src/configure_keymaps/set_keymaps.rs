5f2b92f429914837f89ab9c139f4d1d2cc7065a8
Marcelo <mrcelo014@gmail.com>
2024-05-21 15:20:07 -0300

    set keymaps

--- src/configure_keymaps/set_keymaps.rs
+++ src/configure_keymaps/set_keymaps.rs
@@ -7,16 +7,16 @@ use crate::{
 
 pub fn configure_keymaps() -> Result<(), String> {
     let keymap_selected = get_user_selections(&KEYMAPS, "Selecione o layout de taclado");
-    let path = "/etc/vconsole.conf";
+    // let path = "/etc/vconsole.conf";
 
-    let mut file = match OpenOptions::new().append(true).create(true).open(path) {
-        Ok(file) => file,
-        Err(err) => return Err(format!("Error: {}", err)),
-    };
+    // let mut file = match OpenOptions::new().append(true).create(true).open(path) {
+    //     Ok(file) => file,
+    //     Err(err) => return Err(format!("Error: {}", err)),
+    // };
 
-    if let Err(err) = file.write_all(path.as_bytes()) {
-        return Err(format!("Error: {}", err));
-    }
+    // if let Err(err) = file.write_all(keymap_selected[])) {
+    //     return Err(format!("Error: {}", err));
+    // }
 
     run_command(&mut Command::new("sh").arg("-c").arg(format!(
         "echo KEYMAP={} >> /etc/vconsole.conf",
